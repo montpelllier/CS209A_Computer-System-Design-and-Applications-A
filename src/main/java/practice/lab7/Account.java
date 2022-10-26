d397cd99e6f28f71fa6122b80b9e57e71bcf8ca7
montpelllier <1565975426@qq.com>
2022-10-26 14:09:57 +0800

    initialize lab7

---
+++ src/main/java/practice/lab7/Account.java
@@ -0,0 +1,30 @@
+package practice.lab7;
+
+public class Account {
+
+  private double balance;
+
+  /**
+   * @param money
+   */
+  public void deposit(double money) {
+    try {
+      double newBalance = balance + money;
+      try {
+        Thread.sleep(10);   // Simulating this service takes some processing time
+      } catch (InterruptedException ex) {
+        ex.printStackTrace();
+      }
+      balance = newBalance;
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+  }
+
+
+  public double getBalance() {
+    return balance;
+  }
+}
